1448031df05862accf9199984e474615e095e1eb
Psi <psi.xie@gmail.com>
2013-08-15 15:35:19 +0800

    Update engine.rb

--- lib/taobao/rails/engine.rb
+++ lib/taobao/rails/engine.rb
@@ -3,7 +3,7 @@ module Taobao
     class Engine < ::Rails::Engine
 
       initializer "taobao.top" do |app|
-        ::Taobao::TOP.sandbox = !::Rails.env.production?
+       # ::Taobao::TOP.sandbox = !::Rails.env.production?
 
         if defined?(::OmniAuth::Strategies::Taobao)
           ::OmniAuth::Strategies::Taobao.option :client_options, ::Taobao::TOP.gateways
